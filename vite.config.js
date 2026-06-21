34d1a1ddd3e07e4c944ab9847c17a3ee8cdd62b1
vamsiperla <perla.vamsi@gmail.com>
2026-06-21 12:42:15 -0500

    use github actions to deploy changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/growthdesk/',
+  define: {
+    'window.__GD_GIST_ID__': JSON.stringify(process.env.VITE_GIST_ID || ""),
+    'window.__GD_GIST_TOKEN__': JSON.stringify(process.env.VITE_GIST_TOKEN || ""),
+    'window.__GD_EDIT_SECRET__': JSON.stringify(process.env.VITE_EDIT_SECRET || ""),
+  }
 })
